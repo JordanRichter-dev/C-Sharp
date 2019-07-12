b3ba8b1cbe44af48567d0da346645510f50541d0
Duojet <46941789+Duojet@users.noreply.github.com>
2019-07-12 16:09:08 -0700

    CFApp5 started

---
+++ CFApp5/CFApp5/Migrations/201907122306301_Employee.cs
@@ -0,0 +1,32 @@
+namespace CFApp5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Employee : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        FirstName = c.String(),
+                        LastsName = c.String(),
+                        Email = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.AspNetUsers", "FirstName", c => c.String());
+            AddColumn("dbo.AspNetUsers", "LastName", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "LastName");
+            DropColumn("dbo.AspNetUsers", "FirstName");
+            DropTable("dbo.Employees");
+        }
+    }
+}
